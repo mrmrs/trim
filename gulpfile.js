6b1b310a4c4fbc1c7fab3520d65e61ae9f6a78f6
mrmrs <hi@mrmrs.cc>
2014-01-21 00:05:03 -0800

    Modularizings.

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,53 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     prefix = require('gulp-autoprefixer'),
     minifyCSS = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     imagemin = require('gulp-imagemin'),
+    jshint = require('gulp-jshint'),
     csslint = require('gulp-csslint');
 
+gulp.task('minify-css', function(){
+    gulp.src('css/*.css')
+      .pipe(minifyCSS({keepSpecialComments: 0}))
+      .pipe(gulp.dest('css/'));
+});
 
-gulp.task('default', function(){
+gulp.task('minify-js', function() {
+    gulp.src('./js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/'));
+});
 
-   gulp.src('./sass/*.scss')
+gulp.task('lint', function(){
+  gulp.src('./css/*.css')
+        .pipe(watch(function(files) {
+          return files.pipe(csslint({
+          'compatible-vendor-prefixes': false,
+          'box-sizing': false
+          }))
+         .pipe(csslint.reporter());
+  }));
+});
+
+gulp.task('pre-process', function(){
+  gulp.src('./sass/*.scss')
           .pipe(watch(function(files) {
             return files.pipe(sass({includePaths: ['sass/']}))
-              .pipe(gulp.dest('./css'));
+              .pipe(prefix())
+              .pipe(gulp.dest('css/'));
           }));
+});
 
-    // Lint your css!
-    // Let the computer help you. It knows what to do :)
-   gulp.src('./css/*.css')
-        .pipe(watch(function(files) {
-          return files.pipe(csslint())
-             .pipe(csslint.reporter());
-        }));
+gulp.task('default', function(){
+  gulp.watch('sass/**/*.scss', function(event) {
+    gulp.run('pre-process');
+    gulp.run('lint');
+  });
 });
 
 gulp.task('production', function(){
-
-    gulp.src('css/*.css')
-      .pipe(prefix())
-      .pipe(gulp.dest('css/'));
-
-    // Minify your css!
-    // This removes comments and whitespace. Puts it all on one line.
-    gulp.src('./css/*.css')
-      .pipe(minifyCSS())
-      .pipe(gulp.dest('css/'));
-
-    // Optimize your images for production
-    gulp.src('./img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./img'));
-
+    gulp.run('minify-css');
+    gulp.run('minify-js');
 });
+
