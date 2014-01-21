aa4dbd3a20a5b1d2d410ab1eca8292f25a37f0f4
mrmrs <hi@mrmrs.cc>
2014-01-20 20:06:27 -0800

    Add initial gulpfile and test assets.
    Sass and auto-prefixer are definitely working. Minify css still seems
    a bit buggy.

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    watch = require('gulp-watch'),
+    plumber = require('gulp-plumber'),
+    prefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin'),
+    csslint = require('gulp-csslint'),
+    sass = require('gulp-sass');
+
+
+gulp.task('default', function(){
+
+   gulp.src('./sass/*.scss')
+          .pipe(watch(function(files) {
+            return files.pipe(sass({includePaths: ['sass/']}))
+              .pipe(gulp.dest('./css'));
+          }));
+
+    // Lint your css!
+    // Let the computer help you. It knows what to do :)
+   // gulp.src('./css/*.css')
+   //     .pipe(csslint())
+   //     .pipe(csslint.reporter());
+});
+
+gulp.task('production', function(){
+
+    gulp.src('css/*.css')
+      .pipe(prefix())
+      .pipe(gulp.dest('css/'));
+
+    // Minify your css!
+    // This removes comments and whitespace. Puts it all on one line.
+    gulp.src('./css/*.css')
+      .pipe(minifyCSS())
+      .pipe(gulp.dest('css/'));
+
+    // Optimize your images for production
+    gulp.src('./img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./img'));
+
+});
