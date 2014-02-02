d7233ecf731754a9d472692959bdbec236a920b2
mrmrs <hi@mrmrs.cc>
2014-02-02 15:49:41 -0800

    Watches html for changes and pushes them to livereload server.

--- gulpfile.js
+++ gulpfile.js
@@ -17,43 +17,42 @@ var gulp = require('gulp'),
 
 
 // Task to minify all css files in the css directory
-
 gulp.task('minify-css', function(){
   gulp.src('./css/*.css')
     .pipe(minifyCSS({keepSpecialComments: 0}))
     .pipe(gulp.dest('./css/'));
 });
 
+// Reload html
+gulp.task('reload', function(){
+  gulp.src('*.html')
+    .pipe(watch(function(files) {
+      return files.pipe(livereload(server));
+    }));
+});
 
 // Task to minify all js files in the js directory
-
 gulp.task('minify-js', function() {
   gulp.src('./js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./js/'));
 });
 
-
 // Task to optmize and minify images
-
 gulp.task('minify-img', function() {
   return gulp.src('./img/**/*')
     .pipe((imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
     .pipe(gulp.dest('./img'));
 });
 
-
 // Task to optimize and minify svg
-
 gulp.task('minify-svg', function(){
   gulp.src('./img/svg')
           .pipe(svgmin())
           .pipe(gulp.dest('./img/svg'));
 });
 
-
 // Task to run jshint and pipe output to terminal
-
 gulp.task('jshint', function(){
   gulp.src('./js/*.js')
      .pipe(jshint())
@@ -64,12 +63,12 @@ gulp.task('jshint', function(){
 
 // Use csslint without box-sizing or compatible vendor prefixes (these
 // don't seem to be kept up to date on what to yell about)
-
 gulp.task('csslint', function(){
   gulp.src('./css/*.css')
     .pipe(csslint({
           'compatible-vendor-prefixes': false,
-          'box-sizing': false
+          'box-sizing': false,
+          'important': false
         }))
     .pipe(csslint.reporter());
 
@@ -77,7 +76,6 @@ gulp.task('csslint', function(){
 
 
 // Task that compiles scss files down to good old css
-
 gulp.task('pre-process', function(){
   gulp.src('./sass/*.scss')
       .pipe(watch(function(files) {
@@ -96,19 +94,17 @@ gulp.task('pre-process', function(){
  • Outputted css is run through autoprefixer
  • Runs jshint on all js files in ./js/
  • Sends updates to any files in directory to browser for
- automatic reloading
+   automatic reloading
 
 */
-
 gulp.task('default', function(){
-  gulp.run('pre-process', 'csslint', 'jshint');
+  gulp.run('reload', 'pre-process', 'csslint', 'jshint');
   server.listen(35729, function (err) {
-    gulp.watch(['*.html', './sass/*.scss', './js/*.js'], function(event) {
-      gulp.run('pre-process', 'csslint', 'jshint');
+    gulp.watch(['*.html', '*/*.html', './sass/*.scss', './js/*.js'], function(event) {
+      gulp.run('reload', 'pre-process', 'csslint', 'jshint');
     });
   });
 });
-
 
 gulp.task('production', function(){
     gulp.run('minify-css', 'minify-js', 'minify-img', 'minify-svg');
